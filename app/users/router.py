255ee6dc53c8cc411456333c9cc6615a64919696
kotyara12345678 <savva.toch@gmail.com>
2025-11-02 12:30:28 +0400

    описал router.py

---
+++ app/users/router.py
@@ -0,0 +1,43 @@
+from fastapi import APIRouter, Response
+from fastapi.requests import Request
+from fastapi.responses import HTMLResponse
+from app.exeptions import UserAlreadyExistsException, IncorrectEmailOrPasswordException, PasswordMismatchException
+from app.users.auth import authenticate_user, create_access_token, get_password_hash
+from app.users.dao import UserDAO
+from app.users.schemas import SUserRegister, SUserAuth
+
+router = APIRouter(prefix="/auth", tags=["Auth"])
+
+
+@router.post("/register/")
+async def register_user(user_data: SUserRegister) -> dict:
+    user = await UserDAO.find_one_or_none(email=user_data.email)
+    if user:
+        raise UserAlreadyExistsException
+
+    if user_data.password != user_data.password_check:
+        raise PasswordMismatchException("Пароли не совпадают!")
+    hashed_password = get_password_hash(user_data.password)
+    await UserDAO.add(
+        name=user_data.name,
+        email=user_data.email,
+        hashed_password=hashed_password
+    )
+
+    return {'message': 'Вы успешно зарегистрированы!'}
+
+
+@router.post("/login/")
+async def auth_user(response: Response, user_data: SUserAuth):
+    check = await authenticate_user(email=user_data.email, password=user_data.password)
+    if check is None:
+        raise IncorrectEmailOrPasswordException
+    access_token = create_access_token({"sub": str(check.id)})
+    response.set_cookie(key="user_access_token", value=access_token, httponly=True)
+    return {'ok': True, 'access_token': access_token, 'refresh_token': None, 'message': 'Aвторизация успешна!'}
+
+
+@router.post("/logout/")
+async def logout_user(response: Response):
+    response.delete_cookie(key="user_access_token")
+    return {'message': "Пользователь успешно вышел из системы"}
